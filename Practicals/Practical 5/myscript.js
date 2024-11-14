56c85e7308aabf94162ad9624250cd550888b1bb
Vishesh Dhiman <152509757+Vishesh-Dhiman@users.noreply.github.com>
2024-11-14 09:44:25 +0530

    Add files via upload

---
+++ Practicals/Practical 5/myscript.js
@@ -0,0 +1,19 @@
+function validateForm() { 
+ // Get the form elements 
+ var name = document.getElementById("name").value;
+ var email = document.getElementById("email").value;
+ var age = document.getElementById("age").value;
+ if (name == "") { 
+ alert("Name must be filled out.");
+ return false; 
+ } 
+ if (email == "") { 
+ alert("Email must be filled out.");
+ return false; 
+ } 
+ // Check if the Email is in a valid format 
+ var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+ if (!emailPattern.test(email)) { 
+ alert("Please enter a valid email address.");
+ return false; 
+ }
